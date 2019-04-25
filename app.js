3a4798533e90380575c5f26933f6df6cd2e817ea
Darryn <darryn.patterson@gmail.com>
2019-04-25 23:35:04 +0200

    Changed app port.

--- app.js
+++ app.js
@@ -59,6 +59,6 @@ app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 app.use("/api", require("./routes/api"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5010;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
